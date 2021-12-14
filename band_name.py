087aaf43a236795cfcf90e97b5b7b89100a6549d
jkay2137 <jkay2137@gmail.com>
2021-12-14 13:42:22 +0100

    Added README file and first python project

---
+++ band_name.py
@@ -0,0 +1,8 @@
+print("Hello " + input("What is your name?\n"))
+
+city = input("What is name of the city you grew up in?\n")
+pet_name = input("What is your pet's name?\n")
+
+band_name = city + pet_name
+
+print(f"Name of your band could be {band_name}")
