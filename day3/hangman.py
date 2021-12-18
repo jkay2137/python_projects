b2f01cb3ece3670f4eac4fe897962179c1c2c830
jkay2137 <jkay2137@gmail.com>
2021-12-18 17:01:26 +0100

    Added Hangman ASCII and player lives

--- day3/hangman.py
+++ day3/hangman.py
@@ -1,38 +1,99 @@
+# HANGMAN GAME
 import random
 
+# Hangman ASCII
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+# Variables 
 words_list = ["mouse", "melon", "camel", "letters"]
-
 word = random.choice(words_list)
-
 n = word.__len__()
+lives = 6
 
-print(word)
-
+# Create blanks 
 blanks = []
-
 for char in word:
     if char != " ":
         blanks.append("_")
     else:
         blanks.append(" ")
 
-end = False
-
-while not end:
+# Guessing loop
+while True:
     guess = input("Guess a letter: ").lower()
     i = 0
     while i < n:
         if guess == word[i]:
-            blanks[i] = guess     
+            blanks[i] = guess      
         i += 1
 
-    j = 0
-    for char in blanks:
-        if char == "_":
-            j += 1
-    if j == 0:
-        end = True
+    if word.find(guess) == -1:
+        lives -= 1
+        print(lives)
+    
+    print(' '.join(blanks))   
+    print(stages[lives])
 
-    print(blanks)
+    # When should it end
+    if lives == 0:
+        print("You lose")
+        break
 
-print("You win")
+    if "_" not in blanks:
+        print("You win")
+        break
