a033203b124a0fbb64e618aab27707c81f138ffe
jkay2137 <jkay2137@gmail.com>
2021-12-18 15:00:37 +0100

    Added blanks letters

--- day3/hangman.py
+++ day3/hangman.py
@@ -2,8 +2,24 @@ import random
 
 words_list = ["mouse", "melon", "camel", "letters"]
 
-r = random.randint(0, words_list.__len__() - 1)
-
-word = words_list[r]
+word = random.choice(words_list)
 
 print(word)
+
+blanks = []
+
+for char in word:
+    if char != " ":
+        blanks.append("_")
+
+print(blanks)
+
+guess = input("Guess a letter: ").lower()
+
+i = 0
+while i < word.__len__():
+    if guess == word[i]:
+        blanks[i] = guess
+    i += 1
+
+print(blanks)
