53ef4a9b517dc7970a806f443b9ef7335a4d092f
jkay2137 <jkay2137@gmail.com>
2021-12-20 22:03:25 +0100

    Created one function to encrypt and decrpyt insted of two functions

--- day4/caesar_cipher.py
+++ day4/caesar_cipher.py
@@ -3,29 +3,39 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 
-def encrypt(message, shift):  
-    tab = []
-    for char in message:
-        position = alphabet.index(char)
-        new_position = position + shift
-        while new_position > alphabet.__len__() - 1:
-            new_position -= alphabet.__len__()
-        tab.append(alphabet[new_position])
-    
-    print("".join(tab))
-
-def decrypt(message, shift):
-    tab = []
-    for char in message:
-        position = alphabet.index(char)
-        new_position = position - shift
-        while new_position < 0:
-            new_position += alphabet.__len__()
-        tab.append(alphabet[new_position])
-
-    print("".join(tab))
+def caeser(message, shift, choose):
+    new_text = ""
+    if choose == "encode" or choose == "decode":
+        for char in message:
+            position = alphabet.index(char)
+            if choose == "encode":
+                new_position = position + shift
+                while new_position > alphabet.__len__() - 1:
+                    new_position -= alphabet
+                new_text += alphabet[new_position]
+            else:
+                new_position = position - shift
+                while new_position < 0:
+                    new_position += alphabet.__len__()
+                new_text += alphabet[new_position]
+    if choose == "decode":
+        print(f"The decoded message is:\t {new_text}")
+    elif choose == "encode":
+        print(f"The encoded message is:\t {new_text}")
+    else:
+        print("Wrong option. Try again.")
 
         
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))  
+while True:        
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    caeser(text, shift, direction)
+
+    decision = input("Do you want again? [Y/n]: ").lower()
+
+    if decision == "" or decision == "y":
+        continue
+    else:
+        break  
