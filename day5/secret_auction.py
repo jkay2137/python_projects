087a3e506c04d36963c6e3b20fa76b8a6d0edaa9
jkay2137 <jkay2137@gmail.com>
2022-01-05 22:40:52 +0100

    Added some interface

--- day5/secret_auction.py
+++ day5/secret_auction.py
@@ -1,3 +1,18 @@
 import art
+import os
 
 print(art.logo)
+print("Welcome to the secet auction program.")
+
+clear = lambda: os.system("clear") # Clearing function?
+
+while True:
+    name = input("What is your name?: ")
+    bid = input("What's your bid?: $")
+    bidders = input("Are there any other bidders? [Y/n]\n").lower()
+
+    if bidders != "y" or bidders != "":
+        break
+
+clear()
+       
