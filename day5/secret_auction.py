3a68630d1cd9596d27c6ecc97f93b08653464dc2
jkay2137 <jkay2137@gmail.com>
2022-01-05 23:06:45 +0100

    Finished secret auction file

--- day5/secret_auction.py
+++ day5/secret_auction.py
@@ -6,13 +6,26 @@ print("Welcome to the secet auction program.")
 
 clear = lambda: os.system("clear") # Clearing function?
 
+bidders = {}
+highest = 0
+winner = ""
+
 while True:
     name = input("What is your name?: ")
-    bid = input("What's your bid?: $")
-    bidders = input("Are there any other bidders? [Y/n]\n").lower()
+    bid = int(input("What's your bid?: $"))
+    choice = input("Are there any other bidders? [Y/n]\n").lower()
 
-    if bidders != "y" or bidders != "":
+    bidders[name] = bid
+
+    if choice != "y" and choice != "":
         break
+    clear()
 
 clear()
-       
+
+for key in bidders:
+    if bidders[key] > highest:
+        highest = bidders[key]
+        winner = key
+
+print(f"The winner is {winner} with a bid of ${highest}.")
