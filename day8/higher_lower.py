995e81581870ee3258d67527529a684690379a59
jkay2137 <jkay2137@gmail.com>
2022-04-14 19:30:33 +0200

    Added clearing function, result function and finished the project

--- day8/higher_lower.py
+++ day8/higher_lower.py
@@ -2,6 +2,7 @@
 import art
 import game_data
 import random
+import os
 
 con = True
 data = game_data.data
@@ -24,16 +25,28 @@ def compare(choice):
     elif choice == "B":
         return first_obj["follower_count"] < second_obj["follower_count"]
 
+def result(compare_result):
+    global counter
+    global first_obj
+    global second_obj
+    global con
+    console_clear()
+    if compare_result:
+        counter += 1
+        first_obj = second_obj
+    else:
+        con = False
+        print(art.logo)
+        print("Sorry, that's wrong. Final score: " + str(counter))
+
+def console_clear():
+    os.system("clear")
+
 while con:
     second_obj = data[rand_num(first_obj)]    
     print(art.logo)
-    print(f"Compare A: {first_obj['name']}, {first_obj['description']}, from {first_obj['country']}. {first_obj['follower_count']}")
+    print(f"Compare A: {first_obj['name']}, {first_obj['description']}, from {first_obj['country']}.")
     print(art.vs)
-    print(f"Against B: {second_obj['name']}, {second_obj['description']}, from {second_obj['country']}. {second_obj['follower_count']}")
+    print(f"Against B: {second_obj['name']}, {second_obj['description']}, from {second_obj['country']}.")
     choice = input("Who has more followers? [A/B]: ").upper()
-
-    print(compare(choice))
-
-
-
-    break
+    result(compare(choice))
