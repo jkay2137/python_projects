cf1d7330f2971c7e2e09dac97ecf2450891f3340
jkay2137 <jkay2137@gmail.com>
2022-04-15 22:08:10 +0200

    Finished the coffee machine project using OOP

--- day9/coffee_machine/main.py
+++ day9/coffee_machine/main.py
@@ -5,12 +5,20 @@ from money_machine import MoneyMachine
 money_machine = MoneyMachine()
 coffee_maker = CoffeeMaker()
 menu = Menu()
+con = True
 
-while True:
-    choice = input("What would you like? [espresso/latte/cappucino]: ")
+while con:
+    choice = input(f"What would you like? [{menu.get_items()}]: ")
     if choice == "report":
         coffee_maker.report()
+        money_machine.report()
         continue
+    elif choice == "off":
+        con = False
     else:
-        menu_item = menu.find_drink(choice)
-    break
+        drink = menu.find_drink(choice)
+        if drink != None:
+            if coffee_maker.is_resource_sufficient(drink):
+                if money_machine.make_payment(drink.cost):
+                    coffee_maker.make_coffee(drink)
+    
