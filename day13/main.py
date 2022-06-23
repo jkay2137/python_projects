87cc4e538a78aa633fb0939cf813a4cd725f21d7
jkay2137 <jkay2137@gmail.com>
2022-06-23 23:45:57 +0200

    Added functionality which saves not guessed states into csv file

--- day13/main.py
+++ day13/main.py
@@ -1,3 +1,4 @@
+# U.S. States Game
 import turtle
 import pandas
 
@@ -6,9 +7,9 @@ states = states_data.state.to_list()
 counter = 0
 screen = turtle.Screen()
 screen.title("U.S. State Game")
-good_answers = []    
+good_answers = []
+not_guessed = []    
 img = "blank_states_img.gif"
-df = pandas.DataFrame(good_answers)
 
 screen.addshape(img)
 turtle.shape(img)
@@ -26,13 +27,21 @@ while counter < 50:
     try: 
         answer = turtle.textinput(title=f"{counter}/50 States Correct", prompt="What's another state's name?").title()
     except AttributeError:
-        df.to_csv("answers.csv")
-        exit()
+        break
+
+    if answer == "Exit":
+        break
 
     if answer in states:
         counter += 1
         good_answers.append(answer)
         write(answer)
+    
+for state in states: 
+    if state not in good_answers:
+        not_guessed.append(state)
 
-df.to_csv("answers.csv")
-screen.exitonclick()
+not_guessed_dict = {"States": not_guessed}
+
+df = pandas.DataFrame(not_guessed_dict)
+df.to_csv("states_to_learn.csv")
